570224a9c3419756384eafae47ce6118a57d118a
mzkmnk <sma0729mex@gmail.com>
2024-09-07 11:03:18 +0900

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-ngrx-eslint-template',
+  webDir: 'www'
+};
+
+export default config;
